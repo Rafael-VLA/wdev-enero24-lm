e08ca1bf64507644937c6b4fb9824590cef19a12
Rafael-VLA <rafaelsequeira@vlacademy.com>
2024-04-24 21:51:53 -0600

    07-javascript-inicios: Inicio proyecto

---
+++ 07-javascript-inicios/app.js
@@ -0,0 +1,49 @@
+// Comentario de una sola línea
+// console.log("Esto se mostrará")
+
+
+/* Comentario de varías líneas
+console.log("Esto no debería mostrarse");
+console.log("Esto no debería mostrarse");
+console.log("Esto no debería mostrarse");
+console.log("Esto no debería mostrarse");
+*/
+
+/**/
+
+// undefined
+let nombre;
+
+// Tipo de dato null
+nombre = null;
+
+// Tipo de dato boolean
+nombre = true;
+nombre = false;
+
+// Tipo de dato number
+nombre = 20;
+nombre = 20.2152;
+
+/*
+Java
+
+int miNumero = 20;
+double miNumeo2 = 20.1;
+*/
+
+// Tipo de dato string
+nombre = "Casa";
+
+// Tipo de dato Symbol
+nombre = Symbol('a');
+
+console.log( typeof nombre );
+
+
+let segundoNombre = "Antonio";
+let apellido = "Sequeria";
+let edad = 23;
+let estatura = 1.70;
+
+// const, let y var
