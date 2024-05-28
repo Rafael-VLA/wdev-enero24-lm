1c492f3ee84200cfac924e27e35037768363a004
Rafael-VLA <rafaelsequeira@vlacademy.com>
2024-05-27 22:09:41 -0600

    07-javascript-inicios: Terminado convertidor de divisas

--- 07-javascript-inicios/ConvertidorDivisas/main.js
+++ 07-javascript-inicios/ConvertidorDivisas/main.js
@@ -9,6 +9,9 @@ const inputValorColones = document.getElementById("valor-colones");
 const liEUR = document.getElementById("eur");
 const liUSD = document.getElementById("usd");
 
+const TIPO_CAMBIO_DOLAR_COLONES = 514.26;
+const TIPO_CAMBIO_EURO_COLONES = 558.70;
+
 let monedaSeleccionada = 'USD';
 
 liEUR.addEventListener('click', () => {
@@ -39,7 +42,60 @@ btnMonedaColones.addEventListener('click', () => {
     const valorMonedaParseado = parseFloat(valorMoneda); // parsear el valor el input, de string a number
 
     //              5.4 * 513.04 = x
-    const result = valorMonedaParseado * 513.04;
+    let result = 0;
+
+    // Condicion
+    // if(monedaSeleccionada === 'USD'){
+    //     result = valorMonedaParseado * TIPO_CAMBIO_DOLAR_COLONES; // 513.04
+    // }
+
+    // if(monedaSeleccionada === 'EUR'){
+    //     result = valorMonedaParseado * TIPO_CAMBIO_EURO_COLONES;
+    // }
+
+    // TAREA: Usar switch en lugar de condiciones => tiempo limite: 7:06
+    switch(monedaSeleccionada){
+        case 'USD':
+            result = valorMonedaParseado * TIPO_CAMBIO_DOLAR_COLONES;
+        break;
+
+        case 'EUR': 
+            result = valorMonedaParseado * TIPO_CAMBIO_EURO_COLONES;
+        break;
+
+        default:
+            console.error('Moneda no controlada');
+            // throw new Error('Moneda no controlada'); // dispara un error, terminamos la funcion
+    }
 
     inputValorColones.value = result.toFixed(2); // Muestra el resutado con 2 decimales
-}); 
+});
+
+// Agregar evento a btnColonesMoneda e imprimir hola 
+btnColonesMoneda.addEventListener('click', () => {
+    const colonesValor = inputValorColones.value;
+
+    if(colonesValor.length === 0 || parseInt(colonesValor) < 0){ // si no hay nada en el input o el valor es menor que 0
+        alert('El valor no es correcto');
+        return; // aqui acaba la funcion
+    }
+
+    const valorColonesParseado = parseInt(colonesValor); // pasando a un numero entero
+
+    let result = 0;
+
+    switch (monedaSeleccionada) {
+        case 'USD':
+            result = valorColonesParseado / TIPO_CAMBIO_DOLAR_COLONES; // dolares 
+        break;
+
+        case 'EUR':
+            result = valorColonesParseado / TIPO_CAMBIO_EURO_COLONES; // euros
+        break;
+    
+        default:
+            console.error('Moneda no controlada');
+    }
+
+    inputValorMoneda.value = result.toFixed(2);
+});
---
+++ 07-javascript-inicios/scope.js
@@ -0,0 +1,15 @@
+let nombre = 'Pepito';
+
+function saludar() {
+    let nombre = 'Rafael';
+
+    console.log(`Hola ${nombre}`)
+}
+
+if(true){
+    // let nombre = 'Alonso'
+    console.log(nombre);
+}
+
+
+saludar();
