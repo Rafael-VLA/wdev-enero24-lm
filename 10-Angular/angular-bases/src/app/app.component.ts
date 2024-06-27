166140c2dd9795400df70e80e36503da966d3e10
Rafael-VLA <rafaelsequeira@vlacademy.com>
2024-06-26 21:50:27 -0600

    added angular

---
+++ 10-Angular/angular-bases/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', // nombre de etiqueta al componente
+  templateUrl: './app.component.html', // Path de mi html
+  styleUrl: './app.component.css' // Path de mi css
+})
+export class AppComponent {
+  title = 'angular-bases';
+}
